75cbdc4337c1204d3fae8b8d08b4768dab9a6889
Kevin Arce <iav.kevinarce@ufg.edu.sv>
2023-01-31 20:34:52 -0600

    added CreateUserHandler method

--- routes/users.routes.go
+++ routes/users.routes.go
@@ -3,34 +3,52 @@ package routes
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/KevinArce/Go-Api/db"
+	"github.com/KevinArce/Go-Api/models"
 )
 
 func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode("Hello World")
+	
+	var users []models.User
+	db.DB.Find(&users)
+	json.NewEncoder(w).Encode(&users)
 }
 
 func GetUserHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode("Hello World")
+	json.NewEncoder(w).Encode("Get User Handler")
 }
 
 func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode("Hello World")
+
+	var user models.User
+	json.NewDecoder(r.Body).Decode(&user)
+
+	createdUser := db.DB.Create(&user)
+	err := createdUser.Error
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		json.NewEncoder(w).Encode(err)
+	}
+
+	json.NewEncoder(w).Encode(&user)
 }
 
 func UpdateUserHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode("Hello World")
+	json.NewEncoder(w).Encode("Update User Handler")
 }
 
 func DeleteUserHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode("Hello World")
+	json.NewEncoder(w).Encode("Delete User Handler")
 }
